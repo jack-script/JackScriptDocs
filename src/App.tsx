bb5a55ff5c3c30f3afe687f23ce4ad98b2222215
danielromeo <danielromeo99@gmail.com>
2020-10-17 22:23:31 +0200

    update

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Heloo world</h1>
+    </div>
+  );
+}
+
+export default App;
