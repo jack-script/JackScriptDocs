f689259fae760eccbc383e872ab6edd42c139044
Daniel Mamphekgo <Danielromeo99@gmail.com>
2020-11-08 18:07:33 +0200

    Added PrismJS to package

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import './css/app.scss';
 import Header from './components/header';
 import Footer from './components/footer';
 import Contents from './components/documentation/contents';
+import Prism from 'prismjs';
 
 function App() {
-  return (
-    <div className="App">
-        <Header></Header>
-        <Contents></Contents>
-        <Footer></Footer>
-    </div>
-  );
+	// The code snippet you want to highlight, as a string
+	const code = `var data = 1;`;
+
+	// Returns a highlighted HTML string
+	const html = Prism.highlight(code, Prism.languages.javascript, 'javascript');
+	
+	return (
+		<div className="App">
+			<Header></Header>
+			<Contents></Contents>
+			<Footer></Footer>
+		</div>
+	);
 }
 
 export default App;
