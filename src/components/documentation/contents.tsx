d60ef5da1c58e4b947bf93864f14a5b6031ef849
danielromeo <danielromeo99@gmail.com>
2021-01-23 19:01:08 +0200

    Created a basic final  template design that im using as the final design

--- src/components/documentation/contents.tsx
+++ src/components/documentation/contents.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-// import "../../css/contents.scss";
+import "./contents.scss";
+
 // import Jackstring from "./jackstring";
 import { Collapse, Button, CardBody, Card, Container, Row, Col } from 'reactstrap';
 
@@ -21,10 +22,10 @@ const Contents = (props: any) => {
     }, []);
 
     var styles = {
-        padding: '0px'
+        padding: '0px',
     }
 
-    console.log(Prism);
+    // console.log(Prism);
 
    
 
@@ -33,7 +34,16 @@ const Contents = (props: any) => {
         <Container>
             <Row>
                 <Col md="12">
-                    <h3>Installation</h3>
+                    <h2 className="headerPerSection">Getting started</h2>
+                    <p>This library was written on python version 3.6 . To ue this library install the <a href="https://docs.python.org/3/">latest stable python version</a></p>
+                    <p>This library makes use of python sets from the standard library. Thsi library does not create its own data structure so it is heavily dependent on pythons sets datastructre</p>
+                    <p>See <a href="https://docs.python.org/3.8/tutorial/datastructures.html#sets">python sets Documentation</a> </p>    
+                </Col>
+            </Row>
+
+            <Row>
+                <Col md="12">
+                    <h2 className="headerPerSection">Installation</h2>
                     <p>Install jackscript using your terminal</p>
                     <pre>
                         <code className="language-python">
@@ -51,34 +61,124 @@ const Contents = (props: any) => {
                 </Col>
             </Row>
 
-
+            {/*Sets documentation row*/}
             <Row>
-                <Col md="8" xs="12">
-                    <p></p>
-
+                <Col md="12" xs="12">
+                <h2 className="headerPerSection">Importing the library and using it</h2>
+                    
                   <pre  className=" additionalAttributes" style={styles}>
                       <code className="language-python" style={styles}>
                       {`
-                        set1 = {2, 5, 6}
-                        set2 = {2, 5, 6, 9}
-
-                        answer = isSuperset(set2, set1) # returns True 
-                        print(answer) # prints True
+                        from jackscriptpy import * # import the entire library
+                        # or 
+                        from jackscriptpy import Dom,Range # will import the Dom() and Range() functions
                       `}
                       </code>
                     </pre>
                 </Col>
-            </Row>
-        </Container>
 
-        <style>
-            {`
+                <Col md="12" xs="12">
+                    <h4 className="libraryFunctionHeader">How to create a set:</h4>
+                    <p>Sets are created how you would normally create your set in python; as said earlier This library consnist of a set of functions and doesnt make any new data structures. 
+                    Therefore all set methods from the python standard library will work like usual</p>
+                    <pre  className=" additionalAttributes" style={styles}>
+                        <code className="language-python" style={styles}>
+                        {`
+                       mySet = set()
+                        `}
+                      </code>
+                    </pre>
 
-            `}
-        </style>
+                    <hr />
+                    <h4 className="libraryFunctionHeader">How to create coordinates:</h4>
+                    
+                    <p>Coordinates are created lik so:</p>
+                    <pre  className=" additionalAttributes" style={styles}>
+                        <code className="language-python" style={styles}>
+                        {`
+                        mycoordinate = coord(1, 2) # this sets x to 1 and y to 2
+                        
+                        # add a coordinate to a set, using the coordinate created above:
+                        myset.add(mycoordinate);
 
+                        # as expected, the standard library methods will work as usual
+                        myset.clear()
+                        print(myset)  # returns set()
+                        `}
+                      </code>
+                    </pre>
+                </Col>
+
+                <br />
+                
+                <hr />
+                <Col md="12" xs="12">
+                <h4 id="#isSubset()" className="libraryFunctionHeader">isSubset() function</h4>
+                <p>isSubset() checks whether a set is a subset of another; returns true if parameter one is the subset of paramter two</p>
+                  <pre  className=" additionalAttributes" style={styles}>
+                      <code className="language-python" style={styles}>
+                      {`
+                        # declare 2 sets
+                        set1 = {2, 5, 6}
+                        set2 = {2, 5, 6, 9} 
+
+                        answer = isSubset(set1, set2) # returns True 
+                        print(answer) # prints True
+                      `}
+                      </code>
+                    </pre>
+
+                    <hr />
+
+                    <h4 className="libraryFunctionHeader">isSuperset() function</h4>
+                        <p>simmilary to isSubset() , this function returns true if parameter one is a superset of parameter two</p>
+                        
+                        <pre  className=" additionalAttributes" style={styles}>
+                        <code className="language-python" style={styles}>
+                        {`
+                            set1 = {2, 5, 6}
+                            set2 = {2, 5, 6, 9}
+                            
+                            answer = isSuperset(set2, set1) # returns True 
+                            print(answer) # prints True
+                        `}
+                        </code>
+                        </pre>
+                </Col>
+            </Row>{/*End of sets documentation row*/}
+
+            {/*Properties of sets Row*/}
+            <Row>
+                <Col md="12">
+                    <h2 className="headerPerSection">Conclusion</h2>
+                    <p>To uninstall jackscriptpy <a href="https://pip.pypa.io/en/stable/reference/pip_uninstall/">read these docs</a> on how to uninstall pip packages</p>
+                    <pre>
+                        <code className="language-python">
+                            {`
+                                pip install jackscriptpy
+                            `}
+                        </code>
+                    </pre>    
+                </Col>
+            </Row>{/*End of properties of sets Row*/}
+
+
+            {/*Conclusion Row*/}
+            <Row>
+                <Col md="12">
+                    <h2 className="headerPerSection">Conclusion</h2>
+                    <p>To uninstall jackscriptpy <a href="https://pip.pypa.io/en/stable/reference/pip_uninstall/">read these docs</a> on how to uninstall pip packages</p>
+                    <pre>
+                        <code className="language-python">
+                            {`
+                                pip install jackscriptpy
+                            `}
+                        </code>
+                    </pre>    
+                </Col>
+            </Row>{/*End of conclusion Row*/}
+        </Container> {/*End of main container*/ }
         </div>
-
     );
   }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,60 @@
 import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText } from 'reactstrap';
 import logo from '../images/logo.png';
 
 const Header = () => {
-  const [collapsed, setCollapsed] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
+  const styles = {
+    backgroundColor: '#2d6cdf',
+    padding: '0px'
+  }
+
 
   return (
     <div>
-      <Navbar color="faded" light>
-        <NavbarBrand href="/" className="mr-auto">
-        	<img width="100" height="100" src={logo} />
-        </NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
+      <Navbar style={styles}  dark expand="md">
+        <NavbarBrand href="/">	<img width="60" height="60" src={logo} /></NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
             <NavItem>
               <NavLink href="/components/">Components</NavLink>
             </NavItem>
             <NavItem>
               <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
             </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
           </Nav>
+          <NavbarText>Simple Text</NavbarText>
         </Collapse>
       </Navbar>
     </div>
